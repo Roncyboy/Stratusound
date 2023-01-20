06a094483f77c8d105cf02e4cdb448d105a9c01a
Trevor Lee <tlee297@my.bcit.ca>
2023-01-19 20:19:39 -0800

    get user playlists

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,34 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from "react"
 
 export default function Home() {
   const { data: session } = useSession()
-  console.log(session.token.accessToken)
+  const [playlists, setPlaylists] = useState([])
+
+  const getMyPlaylists = async () => {
+    const res = await fetch('/api/playlists');
+    const { items } = await res.json();
+    setPlaylists(items);
+  };
+
   if (session) {
     return (
       <>
-        Signed in <br />
+        <p>Signed in {session?.token?.email}</p>
         <button onClick={() => signOut()}>Sign out</button>
+
+        <hr />
+
+        <button onClick={() => getMyPlaylists()}>Get playlists</button>
+        <button onClick={() => console.log(playlists.length)}>Playlists</button>
+        {playlists.map((item) => (
+          <div key={item.id}>
+            <h1>{item.name}</h1>
+            <img src={item.images[0]?.url} width="100" />
+          </div>
+        ))}
       </>
     )
   }
